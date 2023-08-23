e0204be277e389e4527791edb2f062819197c21b
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-08-22 22:38:49 -0400

    feat: implement dummy site

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,29 @@ import type { V2_MetaFunction } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Pettengill Violin Studio" },
+    {
+      name: "description",
+      content:
+        "Violinist in the Southern Maine area. Not currently accepting students.",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="h-screen flex flex-col justify-center bg-gradient-to-tr from-slate-100 to-sky-300">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-sky-900 sm:text-5xl">
+          Aaron Pettengill
+        </h1>
+        <h2 className="text-xl font-bold tracking-tight text-sky-700 sm:text-4xl">
+          Violin Teacher
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-slate-500">
+          Not currently accepting students.
+        </p>
+      </div>
     </div>
   );
 }
